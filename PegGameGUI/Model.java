88257076ecb70b5044b3c83a7f56b4bfda6806a5
armaanm25 <ma9322@rit.edu>
2024-03-30 19:12:20 +0400

    Added jar files for GUI

--- PegGameGUI/Model.java
+++ PegGameGUI/Model.java
@@ -4,7 +4,6 @@ package PegGameGUI;
 Implement methods for loading the initial board setup from a file and updating the board state based on user moves.
 Ensure the Model classes follow the MVC pattern and encapsulate the game's state and behavior. */
 
-
 public class Model {
     
 }
