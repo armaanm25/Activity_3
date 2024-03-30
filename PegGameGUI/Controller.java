95f41510bd41335603446da390f436f453dadfd8
armaanm25 <ma9322@rit.edu>
2024-03-30 16:49:38 +0400

    File Outlines based on the MVC architecture

---
+++ PegGameGUI/Controller.java
@@ -0,0 +1,10 @@
+package PegGameGUI;
+
+/* Develop the controller logic to coordinate interactions between the View and Model components.
+Implement methods for responding to user input events and updating the Model accordingly.
+Ensure proper error handling and validation of user actions, such as checking for legal moves. */
+
+
+public class Controller {
+    
+}
---
+++ PegGameGUI/View.java
@@ -0,0 +1,9 @@
+package PegGameGUI;
+
+/* Design the graphical user interface (GUI) for displaying the game board and user interactions.
+Create GUI components such as buttons, labels, and panels to represent the game elements.
+Implement event listeners for mouse clicks to handle user input for selecting and moving pegs. */
+
+public class View {
+    
+}
